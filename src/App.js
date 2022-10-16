e66fea0e17204fc6e0516d0317514c08ccc68b2c
CansinAlkacGok <alkac.cansin@gmail.com>
2022-10-16 18:59:46 +0200

    added some styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
@@ -7,55 +7,57 @@ function App() {
   //const [balance, setBalance] = useState(initialBalance);
   //or
   const [balance, setBalance] = useState(0);
-  const [depositStr, setDepositStr] = useState (0);
+  const [depositStr, setDepositStr] = useState(0);
   const [withdrawalStr, setWithdrawalStr] = useState(0);
 
 
   //on mount, retrieve the balance from localStorage
 
   useEffect(() => { // for retrieving from localStorage but for it we need to use useState(0) not need to use initialBalance
-   const storageBalance = localStorage.getItem('balance');
+    const storageBalance = localStorage.getItem('balance');
 
-    if(storageBalance){
-       setBalance(parseInt(storageBalance))
+    if (storageBalance) {
+      setBalance(parseInt(storageBalance))
     }
   }, []) // this effect will run only one for initial render // to ensure that eseEffect runs only after first initial render
 
-  useEffect(() => { 
+  useEffect(() => {
     localStorage.setItem('balance', balance);
- }, [balance]) // whenever balance changes, this effect will be executed
+  }, [balance]) // whenever balance changes, this effect will be executed
 
-  function updateDepositStr(e){
+  function updateDepositStr(e) {
     setDepositStr(e.target.value);
   };
 
-  function updateWithdrawalStr(e){
+  function updateWithdrawalStr(e) {
     setWithdrawalStr(e.target.value);
   };
 
-  function deposit (){
-    setBalance( balance => balance + parseInt(depositStr))
-    setDepositStr(0); 
+  function deposit() {
+    setBalance(balance => balance + parseInt(depositStr))
+    setDepositStr(0);
   };
 
-  function withdraw (){
-    setBalance( balance => balance - parseInt(withdrawalStr))
-    setWithdrawalStr(0); 
+  function withdraw() {
+    setBalance(balance => balance - parseInt(withdrawalStr))
+    setWithdrawalStr(0);
   }
 
 
   return (
-    
+
     <div>
-      <h1> Your current  balance is {balance} </h1>
 
-      <input onChange= {updateDepositStr} value={depositStr}></input>
-      <button onClick= {deposit}>Deposit</button>
+      <h1>Your current balance is {balance}€</h1>
 
-      <input onChange= {updateWithdrawalStr} value= {withdrawalStr}></input>
-      <button onClick= {withdraw}>Withdraw</button>
-      </div>
-    
+      <input onChange={updateDepositStr} value={depositStr} type='number' />
+      <button onClick={deposit}>Deposit</button>
+
+      <input onChange={updateWithdrawalStr} value={withdrawalStr} type='number' />
+      <button onClick={withdraw}>Withdraw</button>
+
+    </div>
+
   )
 }
 
